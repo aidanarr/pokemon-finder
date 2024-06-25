c094ab2ad90bdbdd01f17fe4e5bc63b9dbc150a6
aidanar <aidanarfer@outlook.es>
2024-06-25 13:08:30 +0200

    add detail loader

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -28,62 +28,55 @@ export const CharacterDetail = ({getPkmData}) => {
     return statList
   }
 
+
   return (
     <>
-      {data ?  <><div className="detail">
-        <article className="detail__card">
-          <p className="card-name">{data.name}</p>
-          <p className="card-pokedex">Pokédex entry #{data.pokedex}</p>
-          <div className="detail__card--sprites">
-            <div>
-              <img src={data.image} />
-              <p>Normal</p>
-            </div>
-            <div>
-              <img src={data.imgShiny} />
-              <p>Shiny</p>
-            </div>
-          </div>
-          {/* <div className="detail__card--info">
-            <p>Type(s): {data.types[0]}{data.types[1] ? ", " + data.types[1] : false}</p>
-            <p>Abilities: {data.abilities[0]}
-              {data.abilities[1] ? ", " + data.abilities[1] : false}
-              {data.abilities[2] ? ", " + data.abilities[2] : false}</p>
-            <p>Weight: {data.weight} kg</p>
-            <div>
-              <p>Base stats: </p>
-              <div>{renderStats(data.stats)}</div>
-            </div>
-          </div> */}
-
-          <div className="detail__card--info">
-            <div className="info-name">
-              <p>Type(s):</p>
-              <p>Abilities:</p>
-              <p>Weight:</p>
+    {data ?  <>
+        <div className="detail">
+          <article className="detail__card">
+            <p className="card-name">{data.name}</p>
+            <p className="card-pokedex">Pokédex entry #{data.pokedex}</p>
+            <div className="detail__card--sprites">
+              <div>
+                <img src={data.image} />
+                <p>Normal</p>
+              </div>
+              <div>
+                <img src={data.imgShiny} />
+                <p>Shiny</p>
+              </div>
             </div>
 
-            <div className="info-values">
-              <p>{data.types[0]}{data.types[1] ? ", " + data.types[1] : false}</p>
-              <p>{data.abilities[0]}
-                {data.abilities[1] ? ", " + data.abilities[1] : false}
-                {data.abilities[2] ? ", " + data.abilities[2] : false}</p>
-              <p>{data.weight} kg</p>
-            </div>
+            <div className="detail__card--info">
+              <div className="info-name">
+                <p>Type(s):</p>
+                <p>Abilities:</p>
+                <p>Weight:</p>
+              </div>
 
-          </div>  
-          <div className="stats">
-              <p className="stats__title">Base stats:</p>
-              <div>{renderStatName(data.stats)}</div>
-              <div>{renderStatValue(data.stats)}</div>
+              <div className="info-values">
+                <p>{data.types[0]}{data.types[1] ? ", " + data.types[1] : false}</p>
+                <p>{data.abilities[0]}
+                  {data.abilities[1] ? ", " + data.abilities[1] : false}
+                  {data.abilities[2] ? ", " + data.abilities[2] : false}</p>
+                <p>{data.weight} kg</p>
+              </div>
+            </div>  
+            <div className="stats">
+                <p className="stats__title">Base stats:</p>
+                <div>{renderStatName(data.stats)}</div>
+                <div>{renderStatValue(data.stats)}</div>
+            </div>
+          </article>
           </div>
-        </article>
-        </div>
-        <Link className="return" to="/">
-          <div className="return__box">
-            <p className="return__box--text">← Back</p>
-          </div>
-        </Link></> : <NotFound />}
+          <Link className="return" to="/">
+            <div className="return__box">
+              <p className="return__box--text">← Back</p>
+            </div>
+          </Link>
+        </> : <><div className="loader-box detail-page">
+            <div className="loader-list"></div>
+          </div></>}
     </>
     
   )
@@ -94,3 +87,5 @@ CharacterDetail.propTypes = {
 };
 
 export default CharacterDetail
+
+/*  */
